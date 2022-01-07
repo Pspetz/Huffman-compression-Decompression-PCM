9b76e4a598146ce89036b7fd10fa6ae8195f3a93
Spetz <73785089+Pspetz@users.noreply.github.com>
2022-01-07 22:57:17 +0200

    Add files via upload

--- Huffman/eff_calc.m
+++ Huffman/eff_calc.m
@@ -1,12 +1,14 @@
 function [out_] = eff_calc( dict, prob )
     % upologizei to meso mikos kwdika
+    disp("meso mikos kwdika:")
     out_ = average_cdl( dict, prob )
     % upologismos tis entropias
     hu = 0;
     for i = 1:length(prob);
-        hu = hu - prob(i)*log2(prob(i))
+        hu = hu - prob(i)*log2(prob(i));
     end
-    out_ = hu/out_
+    disp("apodotikotita kwdikopoihshs:");
+    out_ = hu/out_;
    
     
     
