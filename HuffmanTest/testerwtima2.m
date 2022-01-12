6cc10513149944029d39fde3df02472eb3221bad
Spetz <73785089+Pspetz@users.noreply.github.com>
2022-01-12 05:24:49 +0200

    Add files via upload

--- HuffmanTest/testerwtima2.m
+++ HuffmanTest/testerwtima2.m
@@ -1,13 +1,13 @@
-function [] = testerwtima2()
-
- 
- alphabet = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","space" } ;
- prob = [0.0698,0.0128,0.023,0.0364,0.1086,0.0190,0.0172,0.0521,0.0595,0.0013,0.0066,0.0344,0.0206,0.0577,0.0642,0.0165,0.0008,0.0512,0.0541,0.0774,0.0236,0.0084,0.0202,0.0013,0.0169,0.0006,0.1453];
- dict = Huffmandictionary(alphabet,prob);
+%HUFFMANDICT:
+% while (prob ~=1.0003) 
+%gia na treksei to arxeio edw
+alphabet = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","space" } ;
+prob = [0.0698,0.0128,0.0238,0.0364,0.1086,0.0190,0.0172,0.0521,0.0595,0.0013,0.0066,0.0344,0.0206,0.0577,0.0642,0.0165,0.0008,0.0512,0.0541,0.0774,0.0236,0.0084,0.0202,0.0013,0.0169,0.0006,0.1453];
+dict = Huffmandictionary(alphabet,prob);
 
  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
- disp("String :");
- fileI=fopen('cvxopt.txt','r');
+disp("String :");
+fileI=fopen('cvxopt.txt','r');
 x=fscanf(fileI,"%s");
 counter=1;
 folder={};
@@ -19,7 +19,6 @@ for i=1
     end
     enco=strcat(enco,folder);
 end
-
 encoded = Huffmanencoding(folder,dict);
 disp("Encoding :")
 disp(encoded);
@@ -29,9 +28,7 @@ fid = fopen('C:\Users\User\Documents\MATLAB\Νέος φάκελος\charstobits.
     fprintf(fid,'%s',encoded);
     end
  fclose(fid)
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 decoded = Huffmandecoding(encoded,dict);
 disp("Decoding :")
 disp((decoded));
@@ -43,9 +40,7 @@ fid = fopen('C:\Users\User\Documents\MATLAB\Νέος φάκελος\bittochar.tx
 fclose(fid)
 
 length(encoded)
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 meso_mikos_kwdika=eff_calc(dict,prob);
 disp(meso_mikos_kwdika)
-end
